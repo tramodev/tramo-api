8965e72189d5b87b6075a8780ecb50cbdef581f6
Emart99 <ezequielsimon22@yahoo.com.ar>
2026-07-13 23:49:29 -0300

    changes to secret values

--- src/main/java/com/mypath/backend/security/jwt/JwtService.java
+++ src/main/java/com/mypath/backend/security/jwt/JwtService.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,8 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY="586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
+    @Value("${app.jwt.secret}")
+    private String secretKey;
 
     public String getToken(UserDetails user) {
         return getToken(new HashMap<>(), user);
@@ -36,7 +38,7 @@ public class JwtService {
     }
 
     private Key getKey() {
-        byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes= Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
